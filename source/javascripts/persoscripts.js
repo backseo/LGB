17a85eef2b28f9d02aef17158167096519a3d9cf
serge cleoron <sc21417@gmail.com>
2019-11-11 17:59:34 +0100

    good to show

--- source/javascripts/persoscripts.js
+++ source/javascripts/persoscripts.js
@@ -24,9 +24,19 @@ $(window).scroll(function() {
     $('.navbar-name').css("height", 60);
     $('.navbar-name-brand img').css("height", 60)
     $('tabs-item span img').css("height", 50)
+    $('#ico_cave').css("height", 50)
+    $('#ico_lieu').css("height", 50)
+    $('#ico_contact').css("height", 50)
+    $('#ico_news').css("height", 50)
+    $('#ico_plan').css("height", 50)
   } else {
     $('.navbar-name').css("height", 90);
     $('.navbar-name-brand img').css("height", 85)
     $('tabs-item span img').css("height", 80)
+    $('#ico_cave').css("height", 80)
+    $('#ico_lieu').css("height", 80)
+    $('#ico_contact').css("height", 80)
+    $('#ico_news').css("height", 80)
+    $('#ico_plan').css("height", 80)
   }
 });
